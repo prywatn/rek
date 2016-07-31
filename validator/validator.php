fbe88be426ce77856af022ce102b28b4f197cf0d
wisniewskipawel <wisniewskipawel@users.noreply.github.com>
2016-07-31 07:33:56 +0200

    Update validator.php

--- validator/validator.php
+++ validator/validator.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of validator
- *
- * @author caton
- */
 class validator {
 
     public function numbers($numbers1, $numbers2) {
